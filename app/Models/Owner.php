53078ab03e038d549f757cbd3dd508a1f0435935
Adrien Demarez <swayndev@gmail.com>
2022-02-25 11:41:05 +0100

    creating the Models relationships

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Animal;
+
 class Owners extends Model
 {
     use HasFactory;
+
+    public function hasAnimal()
+    {
+        return $this->hasMany(Animal::class);
+    }
+
 }
